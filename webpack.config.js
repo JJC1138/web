83d14069756de06638d9d80045425711552d779f
Jon Colverson <github@vcxz.co.uk>
2016-02-25 15:56:23 +0000

    Added webpack plugins for minimizing in production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,19 @@
+'use strict';
+
+const webpack = require('webpack');
+
 const outpath = 'site';
+
+const production = process.env.NODE_ENV === 'production';
+
+let plugins = [];
+
+if (production) {
+    plugins = plugins.concat([
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false }}),
+    ]);
+}
 
 module.exports = {
     entry: './source/main.js',
@@ -17,4 +32,7 @@ module.exports = {
     devServer: {
         contentBase: outpath,
     },
+    plugins: plugins,
+    debug: !production,
+    devtool: production ? false : 'eval',
 };
