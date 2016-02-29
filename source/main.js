9cc774a7f7c63973d36bd380504a92880a425ce9
Jon Colverson <github@vcxz.co.uk>
2016-02-29 14:55:32 +0000

    Used webfontloader to load the TypeKit fonts.

--- source/main.js
+++ source/main.js
@@ -1,10 +1,13 @@
 import $ from 'jquery';
+import WebFont from 'webfontloader';
 import 'normalize.css';
 
 import './style.css';
 
 import emailDeobfuscator from './email-deobfuscator.js';
 import flickrBackgroundImage from './flickr-background-image.js';
+
+WebFont.load({ typekit: { id: 'kqf5olx' } });
 
 $(document).ready(() => {
     emailDeobfuscator();
