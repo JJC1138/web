693725eb4561590ae9bb72cf0364cae670b01d6a
Jon Colverson <github@vcxz.co.uk>
2016-03-01 14:37:02 +0000

    Removed jQuery dependency from email deobfuscator.

--- source/email-deobfuscator.js
+++ source/email-deobfuscator.js
@@ -1,14 +1,14 @@
-import $ from 'jquery';
-
 export default () => {
     // This is based on:
     // http://pipwerks.com/2009/02/01/obfuscating-email-addresses-revisited/
     // by Philip Hutchison
-    const events = 'click contextmenu focus mouseover';
+    const events = ['click', 'contextmenu', 'focus', 'mouseover'];
     function addressCleaner() {
         /*eslint no-invalid-this: 0*/
         this.href = this.href.split('?')[0].replace('DELETE@THIS', '@');
-        $(this).unbind(events, addressCleaner);
+        for (const event of events) this.removeEventListener(event, addressCleaner);
     }
-    $('a[href^="mailto:"]').bind(events, addressCleaner);
+    for (const element of [...document.querySelectorAll('a[href^="mailto:"]')]) { // conversion to Array is because NodeList is not yet directly iterable in all browsers (e.g. Chrome 48)
+        for (const event of events) element.addEventListener(event, addressCleaner);
+    }
 }
