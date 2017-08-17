34dcde269e79a82d1ce7a25811006e460047a408
Jon Colverson <github@vcxz.co.uk>
2017-08-17 02:36:27 +0100

    Renamed 'style.css' to 'home.css' because it's specific to the home page

--- source/home.js
+++ source/home.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'normalize.css';
 
-import './style.css';
+import './home.css';
 
 import emailDeobfuscator from './email-deobfuscator.js';
 import flickrBackgroundImage from './flickr-background-image.js';
