caeff933aff635491ea9461fb9bb73615dfff03a
Jon Colverson <github@vcxz.co.uk>
2017-08-21 16:21:22 +0100

    CV: Used nicer fonts (preferring Helvetica Neue when available)

--- source/cv.js
+++ source/cv.js
@@ -2,13 +2,6 @@ import $ from 'jquery';
 import 'normalize.css';
 
 import { cleanAddress, addListenersToCleanAddressesOnInteraction } from './email-deobfuscator.js';
-import loadFonts from './load-fonts.js';
-
-loadFonts(
-    'pnk2egk',
-    () => {},
-    () => { document.body.style.visibility = "visible"; }
-);
 
 $(document).ready(() => {
     addListenersToCleanAddressesOnInteraction();
